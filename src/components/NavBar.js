6b41ccd991ec9be30c2d8af01a839323444bc7fb
Otavio <otaviomorais001@hotmail.com>
2023-09-16 15:24:52 -0300

    Layout estático e componente de classificação por estrela finalizados

---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export function NavBar({ children }) {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>CineCritíco</h1>
+    </div>
+  );
+}
+
+export function Search() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Pesquisar filmes..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+
+export function NumResults({ movies }) {
+  return (
+    <p className="num-results">
+      Foram encontrados <strong>{movies.length}</strong> resultados
+    </p>
+  );
+}
